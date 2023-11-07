4f3ed83fd88f6bfb696581996d4a62d63d35951d
TruongVT <truongvt.Rikkeisoft@vnpt-technology.vn>
2023-11-07 15:06:00 +0700

    implement face recognition

--- app/src/main/java/com/testintergateai/presentaion/screen/recognition_screen/RecognitionScreen.kt
+++ app/src/main/java/com/testintergateai/presentaion/screen/recognition_screen/RecognitionScreen.kt
@@ -1,6 +1,7 @@
 package com.testintergateai.presentaion.screen.recognition_screen
 
 import android.util.Log
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,6 +18,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
 import com.testintergateai.domain.model.FaceAnalyzer
 import com.testintergateai.domain.tempData.TempData
 import com.testintergateai.presentaion.ui.component.CameraView
@@ -62,6 +67,26 @@ fun RecognitionScreen() {
                     }
                 }
             )
+
+            result.forEach {
+                it.rect?.let {
+                    Canvas(modifier = Modifier.fillMaxSize()) {
+                        val paint = Paint()
+                        paint.color = Color.Red
+                        paint.style = PaintingStyle.Stroke
+                        paint.strokeWidth = 5f
+
+                        val rect = Rect(
+                            left = it.left.toFloat(),
+                            right = it.right.toFloat(),
+                            top = it.top.toFloat(),
+                            bottom = it.bottom.toFloat()
+                        )
+
+                        this.drawContext.canvas.drawRect(rect, paint)
+                    }
+                }
+            }
         }
     }
 
